90288a601a736b4d7e49e2e0da7e3aaaf57cab14
pdros <piotr.dros01@gmail.com>
2025-01-25 20:10:36 +0100

    eslint & prettier

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,9 +1,9 @@
-import type { Route } from './+types/home'
+import type { Route } from './auth/+types/login'
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
-export default function Home() {
-  return <div>Hello World</div>
+export default function Login() {
+  return <div>Login</div>
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  includeIgnoreFile(gitignorePath),
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['tailwind.config.ts'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'no-empty-pattern': 'off',
+    },
+  },
+]
